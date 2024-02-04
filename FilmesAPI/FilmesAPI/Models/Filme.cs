130b0bf7af7d87f6b1d12c7fd387f744d0d92c13
Gustavo Carvalho Alves dos Santos <gustavo.carvalhoas@hotmail.com>
2024-02-04 19:13:29 -0300

    @GustavoCarvalho25 - Criação da API de filmes

---
+++ FilmesAPI/FilmesAPI/Models/Filme.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        [Required(ErrorMessage = "Necessário preencher o titulo do filme")]
+        [MaxLength(50, ErrorMessage = "O titulo nao pode ultrapassar 100 caracteres")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "Necessário preencher o genero do filme")]
+        [MaxLength(50, ErrorMessage = "O genero nao pode ultrapassar 50 caracteres")]
+        public string Genero { get; set; }
+
+        [Required(ErrorMessage = "Necessário preencher a duracao do filme")]
+        [Range(70,600, ErrorMessage = "A duracao do filme deve ser entre 70 e 600 minutos")]
+        public int Duracao { get; set; }
+
+        [Required(ErrorMessage = "Necessário preencher o diretor do filme")]
+        [MaxLength(50, ErrorMessage = "O nome do diretor nao pode ultrapassar 100 caracteres")]
+        public string Diretor { get; set; }
+    }
+}
