b3432d8f2d333bc51b1d6790b4f044d00b2a2bab
Gustavo Carvalho Alves dos Santos <gustavo.carvalhoas@hotmail.com>
2024-03-01 19:51:39 -0300

    Mudança de pastas DTOS e adição de Cinema

---
+++ FilmesAPI/FilmesAPI/Data/Dtos/CreateCinemaDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace FilmesAPI.Dtos
+{
+    public class CreateCinemaDto
+    {
+        [Required(ErrorMessage = "Campo Nome obrigatorio.")]
+        [StringLength(50, ErrorMessage = "O nome nao pode ultrapassar 50 caracteres")]
+        [NotNull]
+        public string Nome { get; set; }
+    }
+}
